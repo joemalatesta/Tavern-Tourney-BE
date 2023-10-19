7898930f721f6fb09d664b821db2c70d60bfcaaf
Joe Malatesta <joemalatesta@msn.com>
2023-10-18 21:27:16 -0400

    chang gametype

--- models/match.js
+++ models/match.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const matchSchema = new Schema({
   name: { type: String, unique: true },
-  enum: ['8 Ball', '9 Ball'],
+  gameType: String,
   players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
 }, {
   timestamps: true,
