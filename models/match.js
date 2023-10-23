7a46d9c6568876b0a125e23d21d1af59c3c22152
Joe Malatesta <joemalatesta@msn.com>
2023-10-23 10:33:49 -0400

    add rounds to model

--- models/match.js
+++ models/match.js
@@ -6,7 +6,13 @@ const matchSchema = new Schema({
   name: { type: String, unique: true },
   gameType: String,
   matchPlayers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-  bracketTier: Object,
+  round1:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
+  round2:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
+  round3:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
+  round4:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
+  round5:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
+  round6:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
+
   owner: {type: mongoose.Schema.Types.ObjectId, ref: "Profile"},
 }, {
   timestamps: true,
