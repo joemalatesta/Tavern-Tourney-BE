069fdc9e69e5402e4db4b4099ea5e05c178234da
Joe Malatesta <joemalatesta@msn.com>
2024-02-20 09:34:47 -0500

    add a losers bracket, rounds

--- models/match.js
+++ models/match.js
@@ -6,13 +6,8 @@ const matchSchema = new Schema({
   name: { type: String, unique: true },
   gameType: String,
   matchPlayers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-  round1:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-  round2:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-  round3:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-  round4:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-  round5:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-  round6:[{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
-
+  rounds: Array,
+  loserRounds: Array,
   owner: {type: mongoose.Schema.Types.ObjectId, ref: "Profile"},
 }, {
   timestamps: true,
