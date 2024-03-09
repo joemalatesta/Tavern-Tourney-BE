c8db53a31d96e960dac34477584cb7a7c3e5c4ee
Joe Malatesta <joemalatesta@msn.com>
2024-03-09 12:01:19 -0500

    add win and loss to team model

--- models/team.js
+++ models/team.js
@@ -6,6 +6,8 @@ const teamSchema = new Schema({
   teamName: { type: String, unique: true },
   teamPlayers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
   teamCaptain: String,
+  wins: Number,
+  loss: Number
 }, {
   timestamps: true,
 });
